3b2178b467707d1e373a17a59a6aec55568592b9
jinbin <641336450@qq.com>
2019-05-14 17:21:06 +0800

    delte user

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -61,11 +61,13 @@
                        plain
                        type="primary"
                        icon="el-icon-edit"
+                       @click="editUser(scope.row.id)"
                        circle></el-button>
             <el-button size="mini"
                        plain
                        type="danger"
                        icon="el-icon-delete"
+                       @click="del(scope.row.id)"
                        circle></el-button>
             <el-button size="mini"
                        plain
@@ -107,7 +109,8 @@
         <el-form-item label="手机号"
                       :label-width="formLabelWidth">
           <el-input v-model="form.mobile"
-                    autocomplete="off"></el-input>
+                    autocomplete="off"
+                    @keyup.enter.native="addUser()"></el-input>
         </el-form-item>
 
       </el-form>
@@ -116,6 +119,40 @@
         <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
         <el-button type="primary"
                    @click="addUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 编辑用户 -->
+      <el-dialog title="编辑用户"
+               :visible.sync="dialogFormVisibleSet">
+      <el-form :model="form">
+        <el-form-item label="用户id"
+
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.id"
+                    :disabled=true
+                    autocomplete="off">
+                    </el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.email"
+                    autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.mobile"
+                    autocomplete="off"
+                    @keyup.enter.native="editUser()"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer"
+           class="dialog-footer">
+        <el-button @click="dialogFormVisibleSet = false">取 消</el-button>
+        <el-button type="primary"
+                   @click="editUser()">修 改</el-button>
       </div>
     </el-dialog>
 
@@ -133,8 +170,10 @@ export default {
       usersList: [],
       total: -1,
       dialogFormVisibleAdd: false,
+      dialogFormVisibleSet: false,
       formLabelWidth: '100px',
       form: {
+        id: '',
         username: '',
         password: '',
         email: '',
@@ -146,15 +185,52 @@ export default {
     this.getUserList()
   },
   methods: {
+    // 编辑用户
+    editUser (userid) {
+      this.dialogFormVisibleSet = true
+      this.form.id = userid
+    },
+    // 删除用户
+    del (userid) {
+      this.$confirm('是否删除此用户', '删除用户', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const res = await this.$http.delete(`users/${userid}`)
+        const {meta: {msg, status}} = res.data
+        if (status === 200) {
+          this.$message({
+            type: 'success',
+            message: msg
+          })
+          this.getUserList()
+        } else {
+          this.$message({
+            type: 'info',
+            message: msg
+          })
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
     async addUser () {
       const addres = await this.$http.post('users', this.form)
       const { meta: { msg, status } } = addres.data
       if (status === 201) {
         this.$message.success(msg)
         this.dialogFormVisibleAdd = false
+        this.form.username = ''
+        this.form.password = ''
+        this.form.email = ''
+        this.form.mobile = ''
         this.getUserList()
       } else {
-        this.$message.error('添加失败')
+        this.$message.error(msg)
       }
     },
     searchUserList () {
@@ -181,11 +257,10 @@ export default {
       })
       const { data: { users, total }, meta: { status, msg } } = res.data
       if (status === 200) {
-        this.$message.success(msg)
+        console.log(msg)
         this.total = total
         this.usersList = users
       }
-      console.log(res.data)
     }
   }
 }
